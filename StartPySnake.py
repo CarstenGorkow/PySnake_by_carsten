1285725dfd7b2d1122844637c169aaae658b9633
Carsten Gorkow <CarGo25@gmx.de>
2017-06-06 19:20:45 +0200

    Signed-off-by: Carsten Gorkow <CarGo25@gmx.de>

--- StartPySnake.py
+++ StartPySnake.py
@@ -1,2 +1,50 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+import sys
 
-print("hallo welt")
+import Snake_main_window as design
+
+from PyQt5 import QtGui 
+from PyQt5 import QtWidgets
+from PyQt5 import QtCore
+import PyQt5
+
+#from PyQt5 import QtCore, QtGui, QtWidgets
+#from firstgui import Ui_myfirstgui
+ 
+class MyFirstGuiProgram(design.Ui_MainWindow):
+    def __init__(self, dialog):
+        design.Ui_MainWindow.__init__(self)
+        self.setupUi(dialog)
+ 
+        # Connect "add" button with a custom function (addInputTextToListbox)
+        #self.addBtn.clicked.connect(self.addInputTextToListbox)
+ 
+    #def addInputTextToListbox(self):
+    #    txt = self.myTextInput.text()
+    #    self.listWidget.addItem(txt)
+ 
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    #dialog = QtWidgets.QDialog()
+    dialog = QtWidgets.QMainWindow()
+
+    prog = MyFirstGuiProgram(dialog)
+
+    dialog.show()
+    sys.exit(app.exec_())
+
+
+#class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
+#    def __init__(self, parent=None):
+#        super(ExampleApp, self).__init__(parent)
+#        self.setupUi(self)
+
+#def main():
+#    app = QtGui.QApplication(sys.argv)
+#    form = ExampleApp()
+#    form.show()
+#    app.exec_()
+
+
